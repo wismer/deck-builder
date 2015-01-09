3ae5ad688818576c3ee6a59c5e9aedbd1d44a2a7
Matt Long <matthewhl@gmail.com>
2015-01-08 21:56:53 -0500

    add hook to single expansion

--- app/controllers/expansions_controller.rb
+++ app/controllers/expansions_controller.rb
@@ -1,2 +1,9 @@
 class ExpansionsController < ApplicationController
+  def code
+    @expansion = Expansion.find_by(code: params[:code])
+
+    respond_to do |format|
+      format.json { render json: @expansion }
+    end
+  end
 end
