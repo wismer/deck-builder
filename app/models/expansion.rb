54f866ad012ccd7281ad819cea5b98a70351d8c3
Matt Long <matthewhl@gmail.com>
2015-01-10 13:51:46 -0500

    allow draft collections to be made

--- app/models/expansion.rb
+++ app/models/expansion.rb
@@ -1,3 +1,23 @@
 class Expansion < ActiveRecord::Base
   has_many :cards
+
+  def draft
+    return false if booster.nil?
+
+    card_pool = []
+    i = 0
+    until card_pool.length == booster.length
+      random_card = cards.sample
+      if random_card.rarity.downcase == booster[i]
+        card_pool << random_card
+        i += 1
+      end
+    end
+
+    return card_pool
+  end
+
+  def booster
+    super.split(", ") if super
+  end
 end
